cc5ed5e5f9e914047ad3dcbb767c18a6758574d2
Hannes Van Overloop <hvanoverlo@laas.fr>
2023-07-24 15:47:29 +0200

    code to compute the barycenter of a set of quaternions

---
+++ src/quaternion-barycenter/quatBarycenter.h
@@ -0,0 +1,26 @@
+#include <Eigen/Core>
+#include <Eigen/Eigenvalues>
+#include <iostream>
+
+class QuatBarycenter
+{
+  Eigen::Matrix<double, Eigen::Dynamic, 4> quaternions; // a quaternion per row
+  int nbQuats;
+  Eigen::VectorXd weights; // weights to associate to the quaternions
+  Eigen::Vector4d barycenter; // the average quaternion
+ public:
+  explicit QuatBarycenter(Eigen::Matrix<double, Eigen::Dynamic, 4> data)
+    { nbQuats = int(data.rows());
+      quaternions.resize(nbQuats,4);
+      weights.resize(nbQuats);
+      for (int i=0; i<nbQuats; i++){
+	quaternions.row(i) = (data.row(i)).normalized();
+	weights[i] = 1.;
+      }
+    }
+
+  void computeBarycenter();
+  Eigen::Vector4d getBarycenter() const;
+ private:
+  
+};
